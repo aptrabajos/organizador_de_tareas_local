9123f9a2e33835eeb81e0a06d8dc096d6dfaa0e4
Alejandro Palestrini <apalestrini@gmail.com>
2025-10-10 20:24:25 -0300

    checkpoint: auto after tool use 2025-10-10T20:24:25-03:00

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -21,6 +21,7 @@ fn main() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
         .manage(db)
         .invoke_handler(tauri::generate_handler![
             commands::create_project,
