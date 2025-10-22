ef664f763c7d996d5fbab8fd812069877284f86f
Alejandro Palestrini <apalestrini@gmail.com>
2025-10-22 01:05:52 -0300

    checkpoint: auto after tool use 2025-10-22T01:05:52-03:00

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -15,7 +15,6 @@ import GitCommitModal from './GitCommitModal';
 import ProjectJournal from './ProjectJournal';
 import TodoList from './TodoList';
 import ProjectContext from './ProjectContext';
-import ProjectFilters from './ProjectFilters';
 import {
   openUrl,
   createProjectBackup,
